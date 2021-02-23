5ba1a655cc0216d6ea1c68daa3eb03e24e08665f
hyekyung-kim <khatcong@gmail.com>
2021-02-23 18:03:50 +0900

    clean

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -28,10 +28,10 @@ public class Main {
 
 //        remove: 성공
         it = list.listIterator();
-        while(it.hasNext()){
+        while (it.hasNext()) {
             String person = it.next();
 
-            if(person.equals("Tom")){
+            if (person.equals("Tom")) {
                 it.remove();
             }
         }
@@ -45,89 +45,30 @@ public class Main {
 //        System.out.println(list);
 
 //        add: 성공
-        while(it.hasNext()){
+        while (it.hasNext()) {
             String person = it.next();
-            if(person.equals("Tom")) {
+            if (person.equals("Tom")) {
                 it.add("ADDED");
             }
         }
         System.out.println(list + " " + list.size());
 
 
-//        String[] name = {"Kim", "Jack", "Lee", "Dacuya"};
-//        int i = 0;
-//        for(String Nm :name){
-//            if(Nm.equals("Kim")){
-//                Nm = "PARK";
-//            }
-//            System.out.println("name: " + Nm + " " + name[i]);
-//            i++;
-//        }
-//        for(String Nm :name){
-//            System.out.print(Nm + " ");
-//        }
-//        System.out.println("\nEND");
-        
-        List<Integer> l = Arrays.asList(2,3,6,1,9);
-        l.forEach(p->p*=2);
+        List<Integer> l = Arrays.asList(2, 3, 6, 1, 9);
+        // 변경 X
+        l.forEach(p -> p *= 2);
+        System.out.println(l);
+        // 변경 O
+        l = l.stream().map(p -> p * 2).collect(Collectors.toList());
         System.out.println(l);
 
-        List<Integer> num = Arrays.asList(2,3,6,1,9);
-        num=num.stream().map(p -> p * 2).collect(Collectors.toList());
-        System.out.println(num);
-
-//        it = list.listIterator();
-//        while(it.hasNext()){
-//            int idx = it.nextIndex();
-//            String person = it.next();
-//
-//            System.out.println("Person: " + person + " " + idx);
-//        }
-//
-
-//
-//        System.out.println("\nremove\n");
-//
-//        it = list.listIterator();
-//        while(it.hasNext()){
-//            int idx = it.nextIndex();
-//            String person = it.next();
-//            System.out.println("Person: " + person + " " + idx);
-//        }
-
-
-
-//        System.out.println("\nset & previous\n");
-//        it.set("Belly");
-//
-//        while(it.hasPrevious()){
-//            String person = it.previous();
-//
-//            System.out.println("Person: " + person);
-//        }
-//
-//
-//        System.out.println("\nadd\n");
-//        it.add("new");
-//        it = list.listIterator();
-//        while(it.hasNext()){
-//            int idx = it.nextIndex();
-//            String person = it.next();
-//            System.out.println("Person: " + person + " " + idx);
-//        }
-//
-//        System.out.println("\nprevious\n");
-//        while(it.hasPrevious()){
-//            int idx = it.previousIndex();
-//            String person = it.previous();
-//
-//            System.out.println("Person: " + person + " " + idx);
-//        }
+        List<String> s = Arrays.asList("Kim", "Jack", "Lee", "Dacuya");
+        // 변경 X
+        s.forEach(p -> p = "A");
+        System.out.println(s);
+        // 변경 O
+        s = s.stream().map(p -> p = "A").collect(Collectors.toList());
+        System.out.println(s);
 
     }
-//    public void print(){
-//        for(String name: list){
-//            System.out.println()
-//        }
-//    }
 }
